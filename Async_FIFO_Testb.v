d97cb44199e11c4627cb830142ee67d9c1572651
Md-Faizan1 <132780190+Md-Faizan1@users.noreply.github.com>
2023-06-30 11:55:52 +0530

    Update Async_FIFO_Testb.v
    Improved the variable names

--- Async_FIFO_Testb.v
+++ Async_FIFO_Testb.v
@@ -1,83 +1,85 @@
 `default_nettype none
 `timescale 1ps/1ps
 
-module myAsyncFIFO_TB();
+module AsyncFIFO_TB();
 
-parameter MyDSIZE = 8;
-parameter MyASIZE = 4;
-parameter MyWCLK_PERIOD = 10;
-parameter MyRCLK_PERIOD = 40;
+  parameter DSIZE = 8;
+  parameter ASIZE = 4;
+  parameter WCLK_PERIOD = 10;
+  parameter RCLK_PERIOD = 40;
 
-reg myWreq, myWclk, myWrst_n, myRreq, myRclk, myRrst_n;
-reg [MyDSIZE-1:0] myWdata;
-wire [MyDSIZE-1:0] myRdata;
-wire myWfull, myRempty;
+  reg wreq, wclk, wrst_n, rreq, rclk, rrst_n;
+  reg [DSIZE-1:0] wdata;
+  wire [DSIZE-1:0] rdata;
+  wire wfull, rempty;
 
-// Instance
-myAsyncFIFO
-#(     
-    .MyDepthSize(MyDSIZE),
-    .MyArraySize(MyASIZE)
-)
-u_myAsyncFIFO
-(
-    .myWreq(myWreq), .myWrst_n(myWrst_n), .myWclk(myWclk),
-    .myRreq(myRreq), .myRclk(myRclk), .myRrst_n(myRrst_n),
-    .myWdata(myWdata), .myRdata(myRdata), .myWfull(myWfull), .myRempty(myRempty)
-);
+  // Instance
+  AsyncFIFO #(
+    .DepthSize(DSIZE),
+    .ArraySize(ASIZE)
+  ) u_AsyncFIFO (
+    .wreq(wreq),
+    .wrst_n(wrst_n),
+    .wclk(wclk),
+    .rreq(rreq),
+    .rclk(rclk),
+    .rrst_n(rrst_n),
+    .wdata(wdata),
+    .rdata(rdata),
+    .wfull(wfull),
+    .rempty(rempty)
+  );
 
-initial begin
-    myWrst_n = 0;
-    myWclk = 0;
-    myWreq = 0;
-    myWdata = 0;
-    repeat (2) #(MyWCLK_PERIOD/2) myWclk = ~myWclk;
-    myWrst_n = 1;
-    forever #(MyWCLK_PERIOD/2) myWclk = ~myWclk;
-end
+  initial begin
+    wrst_n = 0;
+    wclk = 0;
+    wreq = 0;
+    wdata = 0;
+    repeat (2) #(WCLK_PERIOD/2) wclk = ~wclk;
+    wrst_n = 1;
+    forever #(WCLK_PERIOD/2) wclk = ~wclk;
+  end
 
-initial begin
-    myRrst_n = 0;
-    myRclk = 0;
-    myRreq = 0;
-    repeat (2) #(MyRCLK_PERIOD/2) myRclk = ~myRclk;
-    myRrst_n = 1;
-    forever  #(MyRCLK_PERIOD/2) myRclk = ~myRclk;
-end
+  initial begin
+    rrst_n = 0;
+    rclk = 0;
+    rreq = 0;
+    repeat (2) #(RCLK_PERIOD/2) rclk = ~rclk;
+    rrst_n = 1;
+    forever  #(RCLK_PERIOD/2) rclk = ~rclk;
+  end
 
-initial 
-begin
-    $dumpfile("dump.vcd"); 
+  initial begin
+    $dumpfile("dump.vcd");
     $dumpvars;
-end  
+  end
 
+  initial begin
+    repeat (4) @(posedge wclk);
+    @(negedge wclk); wreq = 1; wdata = 8'd1;
+    @(negedge wclk); wreq = 1; wdata = 8'd2;
+    @(negedge wclk); wreq = 1; wdata = 8'd3;
+    @(negedge wclk); wreq = 1; wdata = 8'd4;
+    @(negedge wclk); wreq = 1; wdata = 8'd5;
+    @(negedge wclk); wreq = 1; wdata = 8'd6;
+    @(negedge wclk); wreq = 1; wdata = 8'd7;
+    @(negedge wclk); wreq = 1; wdata = 8'd8;
+    @(negedge wclk); wreq = 1; wdata = 8'd9;
+    @(negedge wclk); wreq = 1; wdata = 8'd10;
+    @(negedge wclk); wreq = 1; wdata = 8'd11;
+    @(negedge wclk); wreq = 1; wdata = 8'd12;
+    @(negedge wclk); wreq = 1; wdata = 8'd13;
+    @(negedge wclk); wreq = 1; wdata = 8'd14;
+    @(negedge wclk); wreq = 1; wdata = 8'd15;
+    @(negedge wclk); wreq = 1; wdata = 8'd16;
+    @(negedge wclk); wreq = 0;
 
-initial begin
-    repeat (4) @ (posedge myWclk);
-     @(negedge myWclk); myWreq = 1; myWdata = 8'd1;
-     @(negedge myWclk); myWreq = 1; myWdata = 8'd2;
-     @(negedge myWclk); myWreq = 1; myWdata = 8'd3;
-     @(negedge myWclk); myWreq = 1; myWdata = 8'd4;
-     @(negedge myWclk); myWreq = 1; myWdata = 8'd5;
-     @(negedge myWclk); myWreq = 1; myWdata = 8'd6;
-     @(negedge myWclk); myWreq = 1; myWdata = 8'd7;
-     @(negedge myWclk); myWreq = 1; myWdata = 8'd8;
-     @(negedge myWclk); myWreq = 1; myWdata = 8'd9;
-     @(negedge myWclk); myWreq = 1; myWdata = 8'd10;
-     @(negedge myWclk); myWreq = 1; myWdata = 8'd11;
-     @(negedge myWclk); myWreq = 1; myWdata = 8'd12;
-     @(negedge myWclk); myWreq = 1; myWdata = 8'd13;
-     @(negedge myWclk); myWreq = 1; myWdata = 8'd14;
-     @(negedge myWclk); myWreq = 1; myWdata = 8'd15;
-     @(negedge myWclk); myWreq = 1; myWdata = 8'd16;
-     @(negedge myWclk); myWreq = 0;
+    @(negedge rclk); rreq = 1;
+    repeat (17) @(posedge rclk);
+    rreq = 0;
 
-     @(negedge myRclk); myRreq = 1;
-     repeat (17) @(posedge myRclk);
-     myRreq = 0;
-
-     #100;
-     $finish;
-end
+    #100;
+    $finish;
+  end
 
 endmodule
